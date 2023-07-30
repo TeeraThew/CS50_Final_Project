f1398ff85a9f24473ee8139c9e6dc70b7bd4d393
TeeraThew <135465392+TeeraThew@users.noreply.github.com>
2023-07-31 02:18:45 +0700

    Fix login, register

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ def index():
     # Get user's id of current user
     user_id = session["user_id"]
 
+    # If the user have not logged in yet
     if not user_id:
         return render_template("login.html")
 
@@ -192,29 +193,42 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        # Input validation
+        is_valid = True
+        
+        # Get user input
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        # You can also prevent empty user input on the client-side by adding the "required" attribute to the <input> tag
+        # Ensure username was submitted (This is a server-side validation)
+        if not username:
+            is_valid = False
+            flash("must provide username")
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+        if not password:
+            is_valid = False
+            flash("must provide password")
+            
+        if not is_valid:
+            return render_template("login.html")
 
         # Query database for username
-        username = request.form.get("username")
-        # print(usern)
         rows = cur.execute("SELECT * FROM users WHERE username = :username", {'username': username})
         rows = rows.fetchall()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            is_valid = False
+            flash("invalid username and/or password")
+            return render_template("login.html")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return redirect(url_for("index"))
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -281,27 +295,28 @@ def register():
         # Check if the username already exists within the list "usernames" of dictionaries
         # if any(dict["username"] == username for dict in usernames):
         #   return apology("username already exists", 403)
-        else:
-            hash = generate_password_hash(request.form.get("password"))
-            # Insert user data into database
-            try:
-                with conn:
-                    new_user = cur.execute(
-                        "INSERT INTO users (username, hash) VALUES(:username, :hash)", {'username': username, 'hash': hash}
-                    )
-                    new_user = new_user.fetchall()
-            except:
-                is_valid = False
-                flash("Cannot insert user data")  
-            # Another way is to show an auto-generated error message: 
-            # except Exception as e:
-            #     return apology(str(e))
+        
+        hash = generate_password_hash(request.form.get("password"))
+        # Insert user data into database
+        try:
+            with conn:
+                new_user_cur = cur.execute(
+                    "INSERT INTO users (username, hash) VALUES(:username, :hash)", {'username': username, 'hash': hash}
+                )
+                # Get the row id of the new user after inserted into the table "users"
+                new_user_id = new_user_cur.lastrowid
+        except:
+            is_valid = False
+            flash("Cannot insert user data")  
+        # Another way is to show an auto-generated error message: 
+        # except Exception as e:
+        #     return apology(str(e))
         
         if not is_valid:
             return render_template("register.html", username=username)
         else:
             # Remember which user has logged in
-            session["user_id"] = new_user
+            session["user_id"] = new_user_id
 
             # Render home page
             flash("You have registered!")
